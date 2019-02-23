42ddb23effabeab075176810b42dfd7da20f011a
markmahowald <markmahowald0328@gmail.com>
2019-02-23 12:00:38 -0700

    Built up controller for Film ID search

--- src/main/java/com/skilldistillery/mvcfilmsite/controllers/info/FilmQueryController.java
+++ src/main/java/com/skilldistillery/mvcfilmsite/controllers/info/FilmQueryController.java
@@ -1,24 +1,47 @@
 package com.skilldistillery.mvcfilmsite.controllers.info;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.skilldistillery.film.entities.Film;
 import com.skilldistillery.mvcfilmsite.data.DatabaseAccessor;
 
 @Controller
 public class FilmQueryController {
-	
+
 	@Autowired
 	private DatabaseAccessor filmDB;
-	
-	@RequestMapping(path="home.do", method=RequestMethod.GET)
+
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
 	public ModelAndView landingPage() {
 		ModelAndView mv = new ModelAndView("WEB-INF/Views/index.jsp");
-		
+
 		return mv;
 	}
-	
+
+//	map request for a search by filmId. 
+	@RequestMapping(path = "search.do", params="filmId", method = RequestMethod.GET)
+	public ModelAndView idSearch(@RequestParam(value = "filmId") int id) {
+		ModelAndView mv = new ModelAndView();
+		Film f = filmDB.findFilmById(id);
+		mv.addObject("film", f);
+		return mv;
+
+	}
+
+//	search by param keyWord
+	@RequestMapping(path = "search.do", method = RequestMethod.GET)
+	public ModelAndView keyWordSearch(@RequestParam(value = "keyWord") String keyWord) {
+		ModelAndView mv = new ModelAndView();
+		List<Film> f = filmDB.findFilmByKeyWord(keyWord);
+		mv.addObject("film", f);
+		return mv;
+	}
+//	@RequestMapping(path)
 }
